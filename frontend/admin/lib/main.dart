87118edf38e18dc758905c6f3a103a246d376a42
goodven152 <75521339+goodven152@users.noreply.github.com>
2025-06-05 18:41:27 +0400

    Update main.dart

--- frontend/admin/lib/main.dart
+++ frontend/admin/lib/main.dart
@@ -18,7 +18,7 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 /// ─────────────────────────────────────────────
 /// 1.  Единственный хард-код для доступа
-const _password = "PASSWORD_ADMIN";
+String.fromEnvironment('PASSWORD_ADMIN');
 
 void main() => runApp(const MyApp());
 
